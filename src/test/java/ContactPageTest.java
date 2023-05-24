40534b794e9e632361850712437ec0c60cbd0a31
MoritzSalein <83296020+MoritzSalein@users.noreply.github.com>
2023-05-24 14:36:07 +0200

    Imports organized

--- src/test/java/ContactPageTest.java
+++ src/test/java/ContactPageTest.java
@@ -1,15 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.List;
 
 public class ContactPageTest extends FunctionalTest{
     @Test
